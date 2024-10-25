cb84d64d3e6b3da46a66612482e8e9db5c45d6ed
Hadil255 <hadil.lahar@gmail.com>
2024-10-25 10:23:00 -0400

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,15 @@
+public class GameManager : MonoBehaviour {
+    public List<Circuit> circuits;
+    public List<Vehicle> vehicles;
+    public Circuit currentCircuit;
+    public bool isRaceActive;
+    public int remainingLaps;
+
+    void Start() {
+        // Initialisation
+    }
+
+    void Update() {
+        // Gestion de la course
+    }
+}
