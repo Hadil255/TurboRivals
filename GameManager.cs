52ccdaebc45126afaed4c1279804586ce7290521
Hadil255 <hadil.lahar@gmail.com>
2024-10-25 15:27:21 -0400

    UnityProject

---
+++ GameManager.cs
@@ -0,0 +1,13 @@
+public class GameManager : MonoBehaviour {
+    public List<Circuit> circuits;
+    public List<Vehicle> vehicles;
+    public int remainingLaps;
+
+    void Start() {
+        // Initialisation
+    }
+
+    void Update() {
+        // Gestion de la course
+    }
+}
---
+++ Vehicles.cs
@@ -0,0 +1,15 @@
+public class Vehicle : MonoBehaviour {
+    public float speed;
+    public float maxSpeed;
+    public int boosts;
+    public Vector3 position;
+    public bool isBoosting;
+
+    void Move() {
+        // Appel pour déplacer le véhicule
+    }
+
+    void UseBoost() {
+        // Appel pour utiliser un boost
+    }
+}
