d8cfbd6e2095200792514cc88b8b0914692659c3
Hadil255 <hadil.lahar@gmail.com>
2024-10-25 10:27:09 -0400

    Modification de GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -1,8 +1,6 @@
 public class GameManager : MonoBehaviour {
     public List<Circuit> circuits;
     public List<Vehicle> vehicles;
-    public Circuit currentCircuit;
-    public bool isRaceActive;
     public int remainingLaps;
 
     void Start() {
